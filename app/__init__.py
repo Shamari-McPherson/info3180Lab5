06611c4a78d662c2a791ad9f17ce0c3bb7dafa52
Shamari McPherson <mcphersonshamari1011@gmail.com>
2025-03-24 15:14:36 -0500

    Exercise 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,25 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Configuration
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace with a real secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'uploads/'  # Folder to store uploaded files
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+from .models import db
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
+
+from . import models 
 from app import views
