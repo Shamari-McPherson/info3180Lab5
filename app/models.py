62ab9c8e59cae5f705045075e2f4266cae70ddeb
Shamari McPherson <mcphersonshamari1011@gmail.com>
2025-03-24 14:26:59 -0500

    Exercise 2

--- app/models.py
+++ app/models.py
@@ -1,1 +1,18 @@
 # Add any model classes for Flask-SQLAlchemy here
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
